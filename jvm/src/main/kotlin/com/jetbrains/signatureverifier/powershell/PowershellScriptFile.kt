5d613c63d179d0932f338c23fce842af3c7c9e7b
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-02-09 22:20:03 +0100

    [java] WIP: Verify PowerShell signature

---
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/powershell/PowershellScriptFile.kt
@@ -0,0 +1,34 @@
+package com.jetbrains.signatureverifier.powershell
+
+import com.jetbrains.signatureverifier.SignableFile
+import com.jetbrains.signatureverifier.SignatureData
+import org.jetbrains.annotations.NotNull
+import java.nio.channels.Channels
+import java.nio.channels.SeekableByteChannel
+import java.security.MessageDigest
+
+
+open class PowershellScriptFile : SignableFile {
+  private val script: PowerShellScript
+
+  constructor(@NotNull stream: SeekableByteChannel) {
+    script = PowerShellScript(Channels.newInputStream(stream))
+  }
+
+  override fun GetSignatureData(): SignatureData {
+    val bytes = script.decodeSignatureBlock() ?: return SignatureData.Empty
+
+    return SignatureData(null, bytes)
+  }
+
+  override fun ComputeHash(@NotNull algName: String): ByteArray {
+    val digest = MessageDigest.getInstance(algName)
+    return script.computeDigest(digest)
+  }
+
+  fun GetContentWithoutSignature(): String {
+    return script.contentWithoutSignatureBlock
+  }
+}
+
+
